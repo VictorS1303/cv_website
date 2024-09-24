e1c3558b6261b441adb0571e60d370388b79d4ae
Victor Schweitz <victorschweitz@hotmail.com>
2024-09-24 20:34:54 +0200

    Footer finished

---
+++ src/exports/footer.js
@@ -0,0 +1,30 @@
+export const socials = [
+    {
+        socialChannel: "Github",
+        title: "Github",
+        href: "https://github.com/VictorS1303",
+        target: "_blank",
+        socialChannelIcon: "fa-brands fa-github",
+    },
+    {
+        socialChannel: "LinkedIn",
+        title: "LinkedIn",
+        href: "https://www.linkedin.com/in/victor-schweitz-74244b32b/",
+        target: "_blank",
+        socialChannelIcon: "fa-brands fa-linkedin",
+    },
+    {
+        socialChannel: "CodePen",
+        title: "CodePen",
+        href: "https://codepen.io/Schweitz",
+        target: "_blank",
+        socialChannelIcon: "fa-brands fa-codepen",
+    },
+    {
+        socialChannel: "Medium",
+        title: "My Web Development Articles",
+        href: "https://dev.to/worldwidewebdevelopment",
+        target: "_blank",
+        socialChannelIcon: "fa-brands fa-medium",
+    },
+]
