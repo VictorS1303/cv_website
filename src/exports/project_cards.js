ebd5b6be398cb65adf645a22a92cadfad0b3584e
Victor Schweitz <victorschweitz@hotmail.com>
2024-09-27 20:11:56 +0200

    Finished and fixed a little bit of everything

---
+++ src/exports/project_cards.js
@@ -0,0 +1,29 @@
+
+export const projectCards =
+[
+    {
+        'projectCardImage': 'src/images/project_cards/js_cheatsheet.png',
+        'projectCardNumber': '1',
+        'projectCardName': 'JS Cheat Sheet',
+        'githubLink': 'https://github.com/VictorS1303/js_cheatsheet',
+        'projectWebURL': 'https://jscheatsheet-da.netlify.app/',
+        'target': '_blank'
+    },
+    {
+        'projectCardImage': 'src/images/project_cards/js_cheatsheet.png',
+        'projectCardNumber': '2',
+        'projectCardName': 'JS Cheat Sheet',
+        'githubLink': 'https://github.com/VictorS1303/js_cheatsheet',
+        'projectWebURL': 'https://jscheatsheet-da.netlify.app/',
+        'target': '_blank'
+    },
+    {
+        'projectCardImage': 'src/images/project_cards/js_cheatsheet.png',
+        'projectCardNumber': '3',
+        'projectCardName': 'JS Cheat Sheet',
+        'githubLink': 'https://github.com/VictorS1303/js_cheatsheet',
+        'projectWebURL': 'https://jscheatsheet-da.netlify.app/',
+        'target': '_blank'
+    },
+]
+
---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+// gsap.registerPlugin(ScrollTrigger)
+
+
+// // Navbar //
+// gsap.fromTo('.main-nav',
+//     {
+//         y: -100,
+//         duration: 2,
+//     },
+//     {
+//         y: 0,
+//         duration: 2,
+//     }
+// )
+
+
+// // Section: About //
+
+// gsap.fromTo('.skills-wrapper',
+//     {
+//         scrollTrigger: '.skills-wrapper',
+//         x: 0,
+//         opacity: 0,
+//         duration: 1.5,
+//     },
+//     {
+//         x: 200,
+//         opacity: 1,
+//         duration: 1.5,
+//     },   
+// )
+
+
+// // Section: Skills //
+// gsap.fromTo('.skills-wrapper',
+//     {
+//         x: -400,  // Starting position
+//         opacity: 0,
+//     },
+//     {
+//         x: 0,  // Ending position
+//         opacity: 1,
+//         scrollTrigger: {
+//             trigger: '#skills',
+//             toggleActions: 'restart none restart none', 
+//         }
+//     }
+// );
