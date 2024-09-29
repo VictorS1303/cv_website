f8f1da94b6c82c684cb2c9495b6baf0745a48adb
Victor Schweitz <victorschweitz@hotmail.com>
2024-09-29 19:17:26 +0200

    Cleaned up the code a little

--- src/exports/project_cards.js
+++ src/exports/project_cards.js
@@ -9,21 +9,5 @@ export const projectCards =
         'projectWebURL': 'https://jscheatsheet-da.netlify.app/',
         'target': '_blank'
     },
-    {
-        'projectCardImage': 'src/images/project_cards/js_cheatsheet.png',
-        'projectCardNumber': '2',
-        'projectCardName': 'JS Cheat Sheet',
-        'githubLink': 'https://github.com/VictorS1303/js_cheatsheet',
-        'projectWebURL': 'https://jscheatsheet-da.netlify.app/',
-        'target': '_blank'
-    },
-    {
-        'projectCardImage': 'src/images/project_cards/js_cheatsheet.png',
-        'projectCardNumber': '3',
-        'projectCardName': 'JS Cheat Sheet',
-        'githubLink': 'https://github.com/VictorS1303/js_cheatsheet',
-        'projectWebURL': 'https://jscheatsheet-da.netlify.app/',
-        'target': '_blank'
-    },
 ]
 
